b2535121cd242ebea894c1ecc9c12d50d774557a
Afchavez3702 <180235446+Afchavez3702@users.noreply.github.com>
2024-09-26 20:27:36 -0500

    finish all night mode config

--- noche.js
+++ noche.js
@@ -1,3 +1,20 @@
 function modoOscuro(){
     document.getElementById("body").classList.add("oscuro");
+    document.getElementById("nav").classList.add("dark");
+    document.getElementById("footer").classList.add("dark");
+    document.getElementById("enlace").classList.add("enlace-oscuro");
+    document.getElementById("enlace2").classList.add("enlace-oscuro");
+    document.getElementById("enlace3").classList.add("enlace-oscuro");
+    document.getElementById("enlace4").classList.add("enlace-oscuro");
+    document.getElementById("input").classList.add("dark");
+    document.getElementById("input2").classList.add("dark");
+    document.getElementById("input3").classList.add("dark");
 }
+
+
+
+let cokies="Nuestro sitio web utiliza cokies. Al dar click en aceptar permites su uso";
+alert(cokies);
+
+let tratamientoInfo="La información recopilada en esta página, no será compartida con terceros. Solo se usará para contactarnos contigo";
+alert(tratamientoInfo);
